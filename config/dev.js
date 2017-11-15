cca99edfb70d3ccf3f7cacc5292db7208f8fc347
Janus2585 <bd.yhome@gmail.com>
2017-11-14 22:21:42 -0600

    testing .gitignore

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,5 @@
 //DO NOT COMMIT
 
 module.exports ={
-	apiKey: '49QmogELwMaJ61eUDhNwI66bAXJGRubZ6yimTgla',
-	accessKey: 'AKIAJDRACUWKDDPNUZDQ',
-	secretKey: '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'
+	
 };
