1c0f5504458b5704daa1dbdeec7c7d1899f7ce8f
Janus2585 <bd.yhome@gmail.com>
2017-11-14 22:18:16 -0600

    add API keys to config folder

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+//DO NOT COMMIT
+
+module.exports ={
+	apiKey: '49QmogELwMaJ61eUDhNwI66bAXJGRubZ6yimTgla',
+	accessKey: 'AKIAJDRACUWKDDPNUZDQ',
+	secretKey: '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'
+};
---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+	//environment variables are all caps with underscores, by convention
+	apiKey: process.env.API_KEY,
+	accessKey: process.env.ACCESS_KEY,
+	secretKey: process.env.SECRET_KEY
+};
